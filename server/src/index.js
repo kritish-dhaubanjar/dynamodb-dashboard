eacb335899fc1737bfd2a8e1e1d1e692684478cd
Kritish Dhaubanjar <kritish.dhaubanjar@gmail.com>
2022-09-16 10:51:27 +0545

    feat(api): setup spa serving at /dynamodb/*

--- server/src/index.js
+++ server/src/index.js
@@ -11,9 +11,18 @@ app.use(cors({ origin: "*" }));
 
 app.use(morgan("dev"));
 app.use(express.json());
+
+// api
+app.use("/dynamodb/api", routes);
+
+// assets
 app.use("/dynamodb", express.static(path.join(__dirname, "..", "public")));
 
-app.use("/dynamodb/api", routes);
+// spa
+app.get("/dynamodb/*", (req, res) => {
+  res.sendFile(path.resolve(__dirname, "..", "public", "index.html"));
+});
+
 app.use(errorHandler);
 
 app.listen(8080, () => {
