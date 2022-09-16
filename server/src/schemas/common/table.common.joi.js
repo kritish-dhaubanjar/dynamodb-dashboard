4c27442ac188a4c53e3796ea4fc0e2d2c0813c91
Kritish Dhaubanjar <kritish.dhaubanjar@gmail.com>
2022-09-16 11:00:47 +0545

    (fix): add missing schemas for table

---
+++ server/src/schemas/common/table.common.joi.js
@@ -0,0 +1,44 @@
+import Joi from "joi";
+
+// KeySchema
+export const KeySchemaSchema = Joi.array()
+  .items(
+    Joi.object({
+      AttributeName: Joi.string().required(),
+      KeyType: Joi.string().valid("HASH", "RANGE").required(),
+    })
+  )
+  .has(
+    Joi.object({
+      AttributeName: Joi.string(),
+      KeyType: Joi.string().valid("HASH"),
+    })
+  )
+  .required()
+  .min(1);
+
+// ProvisionedThroughput
+export const ProvisionedThroughputSchema = Joi.object({
+  ReadCapacityUnits: Joi.number().min(1),
+  WriteCapacityUnits: Joi.number().min(1),
+}).required();
+
+// ProjectionSchema
+export const ProjectionSchema = Joi.object({
+  ProjectionType: Joi.string()
+    .valid("ALL", "KEYS_ONLY" /*"INCLUDE"*/)
+    .required(),
+}).required();
+
+// AttributeDefinitions
+export const AttributeDefinitionsSchema = Joi.array()
+  .items(
+    Joi.object({
+      AttributeName: Joi.string().required(),
+      AttributeType: Joi.string()
+        .valid("N", "S", "BOOL", "B", "SS", "NS", "BS")
+        .required(),
+    })
+  )
+  .required()
+  .min(1);
