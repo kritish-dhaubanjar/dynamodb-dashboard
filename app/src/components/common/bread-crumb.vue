118f8f4fdd917a320ae364725a7d8e777ab27dd6
Kritish Dhaubanjar <kritish.dhaubanjar@gmail.com>
2022-11-12 00:57:46 +0545

    feat(app): add darkreader toggle

--- app/src/components/common/bread-crumb.vue
+++ app/src/components/common/bread-crumb.vue
@@ -1,7 +1,11 @@
 <template>
   <div class="row">
     <div class="col-12">
-      <nav style="--bs-breadcrumb-divider: '>'" aria-label="breadcrumb">
+      <nav
+        style="--bs-breadcrumb-divider: '>'"
+        aria-label="breadcrumb"
+        class="d-flex justify-content-between"
+      >
         <ol class="breadcrumb">
           <li class="breadcrumb-item"><a href="/">DynamoDB</a></li>
           <li class="breadcrumb-item">
@@ -11,24 +15,60 @@
             {{ store.table.state.Table.TableName }}
           </li>
         </ol>
+        <div class="d-flex">
+          <i class="bi bi-sun me-1"></i>
+          <div class="form-check form-switch">
+            <input
+              class="form-check-input"
+              type="checkbox"
+              role="switch"
+              @change="(e) => toggleDarkReader(e.target.checked)"
+              :checked="initIsDarkReaderEnabled"
+            />
+            <i class="bi bi-moon"></i>
+          </div>
+        </div>
       </nav>
     </div>
   </div>
 </template>
 
 <script setup lang="ts">
-import { inject } from "vue";
+import { inject, onBeforeMount, ref } from "vue";
 import { useRoute, useRouter } from "vue-router";
+import {
+  enable as enableDarkReader,
+  disable as disableDarkMode,
+} from "darkreader";
 
 const route = useRoute();
 const router = useRouter();
 const store: any = inject("store");
+const initIsDarkReaderEnabled = ref(false);
 
 const cancel = () => {
   if (window.history.length > 1) {
     router.back();
   } else {
     router.push({ name: "home" });
+  }
+};
+
+onBeforeMount(() => {
+  initIsDarkReaderEnabled.value = !!localStorage.getItem("darkreader");
+  toggleDarkReader(initIsDarkReaderEnabled.value);
+});
+
+const toggleDarkReader = (value: boolean) => {
+  if (value) {
+    enableDarkReader({
+      brightness: 90,
+      contrast: 90,
+    });
+    localStorage.setItem("darkreader", value.toString());
+  } else {
+    disableDarkMode();
+    localStorage.removeItem("darkreader");
   }
 };
 </script>
