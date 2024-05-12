2e8983f1da8da8f81a77ac4c7f1fee61e5e54f2a
Kritish Dhaubanjar <kritish.dhaubanjar@gmail.com>
2024-05-13 01:49:09 +0545

    feat: add custom serialize and deserialize for StringSet and NumberSet

--- server/src/utils/dynamodb.js
+++ server/src/utils/dynamodb.js
@@ -1,4 +1,4 @@
-import { pick } from "./object";
+import { get, set, pick } from "lodash";
 import { create as TableSchema } from "../schemas/table.joi";
 import { ProvisionedThroughput } from "../constants/dynamodb";
 
@@ -16,4 +16,61 @@ export function constructSchema(Table) {
   }
 
   return schema;
+}
+
+export function serialize(Item, path = "", callback = () => {}) {
+  if (["string", "number", "boolean"].includes(typeof Item) || Item === null) {
+    return Item;
+  }
+
+  if (Item instanceof Set) {
+    callback(path);
+    return Array.from(Item);
+  }
+
+  if (Item instanceof Array) {
+    return Item.map((value, index) => serialize(value, `${path}[${index}]`, callback));
+  }
+
+  const output = {};
+
+  Object.entries(Item).forEach(([key, value]) => {
+    const currentPath = path ? `${path}.${key}` : key;
+
+    if (value instanceof Set) {
+      callback(currentPath);
+      output[key] = Array.from(value);
+      return;
+    }
+
+    if (value instanceof Array) {
+      output[key] = value.map((item, index) => serialize(item, `${currentPath}[${index}]`, callback));
+      return;
+    }
+
+    if (value instanceof Object) {
+      output[key] = serialize(value, currentPath, callback);
+      return;
+    }
+
+    output[key] = value;
+  });
+
+  return output;
+}
+
+export function deserialize(source = {}, object = {}) {
+  const paths = [];
+
+  serialize(source, "", (path) => paths.push(path));
+
+  paths.forEach((path) => {
+    const value = get(object, path);
+
+    if (value instanceof Array) {
+      set(object, path, new Set(value));
+    }
+  });
+
+  return object;
 }
