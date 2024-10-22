ad6e9a14051e96f4094e7a055d6649513151a281
maskeynihal <26411488+maskeynihal@users.noreply.github.com>
2024-10-22 23:40:10 +0545

    chore: fix design issue on pagination link

--- app/src/components/app/table-paginate.vue
+++ app/src/components/app/table-paginate.vue
@@ -1,64 +1,62 @@
 <template>
   <div class="d-flex align-items-center justify-content-end">
-    <div class="d-flex">
-      <nav aria-label="Page navigation example">
-        <ul class="pagination pagination-sm">
-          <li
-            class="page-item"
-            v-for="page in pages"
-            :key="page"
-          >
-            <RouterLink
-              class="page-link rounded-0"
-              :class="{
-                active: store.ui.state.table.page === page,
-                disabled: store.ui.state.isLoading,
-              }"
-              :to="{ name: 'home', query: { ...route.query, page } }"
-            >
-              {{ page }}
-            </RouterLink>
-          </li>
-          <li
-            class="page-item"
-            :class="{ disabled: store.ui.state.isLoading }"
-            v-if="store.dynamodb.state.ExclusiveStartKey"
-          >
-            <a
-              class="page-link rounded-0"
-              href="#"
-              @click.prevent="emit('next')"
-            >
-              ...
-            </a>
-          </li>
-        </ul>
-      </nav>
-
-      <div class="dropdown ms-3">
-        <button
-          class="btn btn-secondary dropdown-toggle btn-sm rounded-0"
-          type="button"
-          data-bs-toggle="dropdown"
-          aria-expanded="false"
+    <nav aria-label="Page navigation example">
+      <ul class="pagination pagination-sm m-0">
+        <li
+          class="page-item"
+          v-for="page in pages"
+          :key="page"
         >
-          {{ limit }} items
-        </button>
-        <ul class="dropdown-menu rounded-0">
-          <li
-            v-for="limit in [10, 25, 50, 100, 200, 300, 500]"
-            :key="limit"
+          <RouterLink
+            class="page-link rounded-0"
+            :class="{
+              active: store.ui.state.table.page === page,
+              disabled: store.ui.state.isLoading,
+            }"
+            :to="{ name: 'home', query: { ...route.query, page } }"
           >
-            <RouterLink
-              class="dropdown-item"
-              :to="{ name: 'home', query: { ...route.query, limit } }"
-              :class="{ active: limit === store.dynamodb.state.Limit }"
-            >
-              {{ limit }} items
-            </RouterLink>
-          </li>
-        </ul>
-      </div>
+            {{ page }}
+          </RouterLink>
+        </li>
+        <li
+          class="page-item"
+          :class="{ disabled: store.ui.state.isLoading }"
+          v-if="store.dynamodb.state.ExclusiveStartKey"
+        >
+          <a
+            class="page-link rounded-0"
+            href="#"
+            @click.prevent="emit('next')"
+          >
+            ...
+          </a>
+        </li>
+      </ul>
+    </nav>
+
+    <div class="dropdown ms-3">
+      <button
+        class="btn btn-secondary dropdown-toggle btn-sm rounded-0"
+        type="button"
+        data-bs-toggle="dropdown"
+        aria-expanded="false"
+      >
+        {{ limit }} items
+      </button>
+      <ul class="dropdown-menu rounded-0">
+        <li
+          v-for="limit in [10, 25, 50, 100, 200, 300, 500]"
+          :key="limit"
+        >
+          <RouterLink
+            class="dropdown-item"
+            :to="{ name: 'home', query: { ...route.query, limit } }"
+            :class="{ active: limit === store.dynamodb.state.Limit }"
+          >
+            {{ limit }} items
+          </RouterLink>
+        </li>
+      </ul>
     </div>
   </div>
 </template>
