f4a97652cfaf31c7e1fc0b025d2862500dcde04f
Kritish Dhaubanjar <kritish.dhaubanjar@gmail.com>
2025-06-10 21:14:48 +0545

    Bump verison v1.22.6 -> v1.22.7

--- Makefile
+++ Makefile
@@ -1,7 +1,7 @@
 NPM=npm
 YARN=yarn
 NODE=node
-VERSION=1.22.6
+VERSION=1.22.7
 APP=dynamodb-dashboard
 # hub.docker.com
 USERNAME=kritishdhaubanjar
--- app/src/App.vue
+++ app/src/App.vue
@@ -5,7 +5,7 @@
     <RouterView />
     <div class="position-fixed bottom-0 end-0 z-3">
       <h6>
-        <span class="badge text-bg-secondary m-4">v1.22.6</span>
+        <span class="badge text-bg-secondary m-4">v1.22.7</span>
       </h6>
     </div>
   </div>
